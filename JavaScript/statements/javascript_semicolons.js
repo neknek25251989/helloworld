c77c713ddca578aca112e0080e5287e3e95fa3e0
Lo Whai Jer <neknek25251989@gmail.com>
2017-11-30 16:07:54 +0800

    [mod] add forggoten semicolon

--- JavaScript/statements/javascript_semicolons.js
+++ JavaScript/statements/javascript_semicolons.js
@@ -2,7 +2,7 @@ var a, b, c;
 a = 1;
 b = 2;
 c = a + b;
-document.getElementById("demo1").innerHTML = c
+document.getElementById("demo1").innerHTML = c;
 var x, y, z; 
 x = 1; y = 2; z = x + y;
-document.getElementById("demo2").innerHTML = z
+document.getElementById("demo2").innerHTML = z;
