2a2e5cd7e5fa282948d90b554bc9e59471591487
Lo Whai Jer <neknek25251989@gmail.com>
2017-12-01 10:33:05 +0800

    [add] Exercises about JavaScript operators

---
+++ JavaScript/operators/javascript_opertors.js
@@ -0,0 +1,6 @@
+var x = 5;
+var y = 2;
+var z = x + y;
+var w = x * y;
+document.getElementById("demo1").innerHTML = z;
+document.getElementById("demo2").innerHTML = w;
---
+++ JavaScript/operators/javascript_string_opertors.js
@@ -0,0 +1,7 @@
+var txt1 = "John";
+var txt2 = "Doe";
+var txt3 = "What a very";
+txt3 += "nice day";
+document.getElementById("demo1").innerHTML = 
+txt1 + " " + txt2;
+document.getElementById("demo2").innerHTML = txt3;
