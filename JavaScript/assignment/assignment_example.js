71a621592907ecfc4a4a5aa70a30bad713ce8fe0
Lo Whai Jer <neknek25251989@gmail.com>
2017-12-04 10:48:51 +0800

    [add] Exercise about JavaScript assignment

---
+++ JavaScript/assignment/assignment_example.js
@@ -0,0 +1,22 @@
+var a = 10;
+document.getElementById("demoa").innerHTML = a;
+
+var b = 10;
+b += 5;
+document.getElementById("demob").innerHTML = b;
+
+var c = 10;
+c -= 5;
+document.getElementById("democ").innerHTML = c;
+
+var d = 10;
+d *= 5;
+document.getElementById("demod").innerHTML = d;
+
+var e = 10;
+e /= 5;
+document.getElementById("demoe").innerHTML = e;
+
+var x = 10;
+x %= 5;
+document.getElementById("demox").innerHTML = x;
